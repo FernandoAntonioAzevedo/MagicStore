4c25bfeacf50900363fcfa706d4822bdf84708d6
FernandoAntonioAzevedo <beratechsolutions@gmail.com>
2025-04-01 02:47:10 -0300

    avançando em responsividade e inserindo animações utilizando Scroll Reveal

--- assets/js/app.js
+++ assets/js/app.js
@@ -48,3 +48,40 @@ var swiper = new Swiper(".mySwiper", {
       },
     },
   });
+
+  // Animations
+  ScrollReveal().reveal(".top_nav", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+  })
+
+  ScrollReveal().reveal(".nav", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    delay: 100,
+  })
+
+  ScrollReveal().reveal(".header", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    delay: 200,
+  })
+
+  ScrollReveal().reveal(".section", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    duration: 1000,
+    delay: 100,
+  })
+
+  ScrollReveal().reveal(".footer", {
+    origin: "bottom",
+    distance: "20px",
+    opacity: 0,
+    duration: 1000,
+    delay: 100,
+  })
